ae0294efe04aad913fada678ca59b92bf01da545
fanyoufu <fanyoufu@outlook.com>
2020-01-17 16:47:43 +0800

    文章删除

--- web_back/js/util/article.js
+++ web_back/js/util/article.js
@@ -8,16 +8,9 @@ var article = {
     get:function(page,type,status){
         return $.get(APILIST.article_get,{'page': page,'type':type, 'state': status})
     },
-    // 添加文章类型
-    // add:function(name,slug){
-    //     return $.post(APILIST.category_add,{'name':name ,'slug':slug })
-    // },
-    // // 删除文章类型
-    // del:function(id){
-    //     return $.post(APILIST.category_del,{'id': id})
-    // },
-    // // 编辑文章类型
-    // edit:function(id,name,slug){
-    //     return $.post(APILIST.category_edit,{'id':id,'name':name ,'slug':slug })
-    // }
+    // 删除文章
+    del:function(id){
+        return $.get(APILIST.article_del,{'id': id})
+    }
+
 }
--- web_back/js/util/config.js
+++ web_back/js/util/config.js
@@ -21,5 +21,7 @@ var APILIST = {
 
 
     // 获取文章
-    article_get: BASEURL + '/admin/search'
+    article_get: BASEURL + '/admin/search',
+    // 删除文章
+    article_del: BASEURL + '/admin/article_delete'
 }
