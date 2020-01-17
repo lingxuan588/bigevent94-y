00776392dc88f0674bcd1025e41c4a7b1d9f8dd6
fanyoufu <fanyoufu@outlook.com>
2020-01-17 14:07:41 +0800

    完成了文章类别编辑功能

--- web_back/js/util/category.js
+++ web_back/js/util/category.js
@@ -12,5 +12,9 @@ var category = {
     // 删除文章类型
     del:function(id){
         return $.post(APILIST.category_del,{'id': id})
+    },
+    // 编辑文章类型
+    edit:function(id,name,slug){
+        return $.post(APILIST.category_edit,{'id':id,'name':name ,'slug':slug })
     }
 }
