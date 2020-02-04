f5475f86c7683663651efe92f60beb565a90714f
lingxuan588 <1096121491@qq.com>
2020-02-04 17:57:46 +0800

    管理员登录页面完成

--- web_back/js/util/config.js
+++ web_back/js/util/config.js
@@ -2,7 +2,10 @@
 // 把这个项目中所有用到的接口地址全放在这里
 
 // 所有接口的基地址
-var BASEURL = 'http://192.168.172.82:8000'
+//老师的端口号
+// var BASEURL = 'http://192.168.172.82:8000'
+// var BASEURL = 'http://192.168.1.104:8000';
+var BASEURL = 'http://192.168.43.101:8000';
 
 // 用来保存所有的接口信息
 var APILIST = {
@@ -11,13 +14,13 @@ var APILIST = {
     user_getInfo: BASEURL + '/admin/getuser',
 
     // 获取文章分类
-    category_get:  BASEURL + '/admin/category_search',
+    category_get: BASEURL + '/admin/category_search',
     // 添加文章分类
-    category_add:  BASEURL + '/admin/category_add',
+    category_add: BASEURL + '/admin/category_add',
     // 删除文章分类
-    category_del:  BASEURL + '/admin/category_delete',
+    category_del: BASEURL + '/admin/category_delete',
     // 编辑文章分类
-    category_edit:  BASEURL + '/admin/category_edit',
+    category_edit: BASEURL + '/admin/category_edit',
 
 
     // 获取文章
